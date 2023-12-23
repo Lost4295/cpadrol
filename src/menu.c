571f8e8def5e783bdd8caa3170c279dff85bd400
LumineWollah <mathis.vareilles@yahoo.com>
2023-12-23 17:09:52 +0100

    add functionning menu

--- src/menu.c
+++ src/menu.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+void choices() {
+    printf("1. Serveur\n");
+    printf("2. Client\n");
+    printf("3. Quitter\n");
+    printf("Votre choix : ");
+}
 
 // Truc rapide, mais à améliorer quoi 
 int main(int argc, char *argv[])
 {
     int choix;
-    printf("1. Serveur\n");
-    printf("2. Client\n");
-    printf("3. Quitter\n");
-    printf("Votre choix : ");
-    scanf("%d", &choix);
-    switch(choix)
-    { //TODO mettre les liens vers les fichiers
-        case 1:
-            printf("Vous avez choisi Serveur\n");
-            break;
-        case 2:
-            printf("Vous avez choisi Client\n");
-            break;
-        case 3:
-            printf("Vous avez choisi Quitter\n");
-            break;
-        default:
-            printf("Choix invalide\n");
-            break;
+    while(true) {
+        choices();
+        scanf("%d", &choix);
+        switch(choix)
+        { 
+            case 1:
+                printf("Vous avez choisi Serveur\n");
+                time.sleep(2);
+                system("cls")                
+                system("p4s.exe")
+                break
+            case 2:
+                printf("Vous avez choisi Client\n");
+                time.sleep(2);
+                system("cls")
+                system("p4c.exe")
+                break
+            case 3:
+                printf("Vous avez choisi Quitter\n");
+                time.sleep(2);
+                break;
+        }
     }
     return 0;
 }
