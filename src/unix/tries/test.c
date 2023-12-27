0719414570fb438a3d40bb3e16f6cc02f426fece
Lost4295 <turin-ylan@outlook.fr>
2023-12-27 20:37:12 +0100

    work on logic of menu

--- src/unix/tries/test.c
+++ src/unix/tries/test.c
@@ -1,9 +1,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <curses.h>
 
 int main(void)
 {
+    initscr();
+    cbreak();
+    noecho();
     char **options[4];
     char *play = "Play";
     char *multiplayer = "Multiplayer";
@@ -13,34 +17,52 @@ int main(void)
     options[1] = &multiplayer;
     options[2] = &settings;
     options[3] = &quit;
-int select = 0;
-char * choose;
-int exit = 0;
+    int select = 0;
+    char *choose;
+    int exit = 0;
+    printf("hi!");
     while (!exit)
     {
         for (int i = 0; i < 4; i++)
         {
-            if (select)
+            if (select == i)
             {
-                printf("->");
+                printw("->");
             }
-            printf("%s\n", *options[i]);
+            else
+            {
+                printw("  ");
+            }
+            printw(" %s\n", *options[i]);
         }
-        scanf("%c", choose);
-        switch (*choose)
-        {
-        case 'u':
-            (select==3)?select=0:select++;
-            break;
-        case 'd':
-            (select==0)?select=3:select--;
-        break;
-        case 'e':
-            exit=1; 
-            break;
-        default:
-            break;
+        if (getch() == '\033')
+        {            // if the first value is esc
+            getch(); // skip the [
+            switch (getch())
+            { // the real value
+            case 'A':
+                // code for arrow up
+                (select == 0) ? select = 3 : select--;
+                clear();
+                break;
+            case 'B':
+                // code for arrow down
+                (select == 3) ? select = 0 : select++;
+                clear();
+                break;
+            case 'C':
+                printw("Option selected : %s\n\n", *options[select]);
+                break;
+            case 'D':
+                // code for arrow left
+                endwin();
+                exit = 1;
+                break;
+            default:
+                break;
+            }
         }
     }
+    endwin();
     return 0;
 }
