0e0d02714cd1ab2181127a698434fb32b7850948
Lost4295 <turin-ylan@outlook.fr>
2024-01-25 13:51:18 +0100

    fix  compiler for windows

--- compileW.sh
+++ compileW.sh
@@ -5,5 +5,5 @@ gcc replayer.c -o ../replayer
 gcc bn.c -o ../menu -lncursesw
 gcc Puissance4réseauC.c -o ../p4c -lSDL2_net -lSDL2 
 gcc Puissance4réseauS.c -o ../p4s -lSDL2_net -lSDL2 -lws2_32
-gcc menu.c  -ISDL2/include -LSDL2/lib -w -lmingw32 -lSDL2main -lSDL2_ttf -lSDL2_mixer -lSDL2_net -lSDL2_image -lws2_32 -o ../main -fstack-protector-all
+gcc menu.c -ISDL2/include -LSDL2/lib -w -o ../main -lmingw32 -lSDL2main -lSDL2  -lSDL2_ttf -lSDL2_mixer -lSDL2_net -lSDL2_image -lws2_32 -fstack-protector-all
 cd ../../
