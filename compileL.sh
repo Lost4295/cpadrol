6c7d8eea444fc127e61db956167e397315906875
Lost4295 <turin-ylan@outlook.fr>
2024-01-25 09:45:26 +0100

    Update compileL.sh and compileW.sh scripts

--- compileL.sh
+++ compileL.sh
@@ -6,5 +6,5 @@ gcc bn.c -o ../menu -lncurses
 gcc Puissance4réseauC.c -o ../p4c -lSDL2_net -lSDL2
 gcc Puissance4réseauS.c -o ../p4s -lSDL2_net -lSDL2
 gcc menu.c $(sdl2-config --cflags --libs)  -lSDL2_ttf -lSDL2_mixer -lSDL2_net -lSDL2_image -o ../main
-cd ../
+cd ../../
 
--- compileW.sh
+++ compileW.sh
@@ -5,6 +5,5 @@ gcc replayer.c -o ../replayer
 gcc bn.c -o ../menu -lncursesw
 gcc Puissance4réseauC.c -o ../p4c -lSDL2_net -lSDL2 
 gcc Puissance4réseauS.c -o ../p4s -lSDL2_net -lSDL2 -lws2_32
-gcc menu.c $(sdl2-config --cflags --libs)  -lSDL2_ttf -lSDL2_mixer -lSDL2_net -lSDL2_image -lws2_32 -o ../main -fstack-protector-all
-cd ../
-
+gcc menu.c  -ISDL2/include -LSDL2/lib -w -lmingw32 -lSDL2main -lSDL2_ttf -lSDL2_mixer -lSDL2_net -lSDL2_image -lws2_32 -o ../main -fstack-protector-all
+cd ../../
